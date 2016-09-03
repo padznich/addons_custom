35a1526ef14b5eb098bbd5ced5b2385cf60af6ab
padznich <slabkopg@gmail.com>
2016-09-03 12:48:14 +0300

    translate Phases

--- project_phases_catalog/models/models.py
+++ project_phases_catalog/models/models.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields
+from openerp.tools.translate import _
 
 
 class CatalogPhases(models.Model):
     _name = 'catalog.phases'
 
-    name = fields.Char(required=True, string='Phase Name')
+    name = fields.Char(required=True, string=_('Phase Name'))
     color = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7')],
                              default=('1', '1'),
-                             string='Color')
+                             string=_('Color'))
     priority = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')],
                                 default=('1', '1'),
-                                string='Priority')
-    description = fields.Char(string='Description')
+                                string=_('Priority'))
+    description = fields.Char(string=_('Description'))
--- project_phases_catalog/models/project_extend.py
+++ project_phases_catalog/models/project_extend.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields
+from openerp.tools.translate import _
 
 
 class ProjectPhaseField(models.Model):
@@ -8,5 +9,4 @@ class ProjectPhaseField(models.Model):
     _inherit = 'project.project'
 
     phase_id = fields.Many2many(comodel_name='catalog.phases',
-                                column1='name', column2='color', string='Phase')
-    # phase_id = fields.Many2many('catalog.phases', string='Phase')
+                                column1=_('name'), column2=_('color'), string=_('Phase'))
