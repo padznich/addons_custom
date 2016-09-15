c6b8685fb4a3c3888069922025983a202d1c763f
padznich <slabkopg@gmail.com>
2016-09-15 21:07:08 +0300

    kostil

--- project_phases_catalog/models/phase.py
+++ project_phases_catalog/models/phase.py
@@ -9,10 +9,10 @@ class CatalogPhases(models.Model):
 
     name = fields.Char(required=True, string=_('Phase Name'))
     color = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5'), ('6', '6'), ('7', '7')],
-                             default=('1', '1'),
+                             default='1',
                              string=_('Color'))
     priority = fields.Selection([('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')],
-                                default=('1', '1'),
+                                default='1',
                                 string=_('Priority'))
     description = fields.Char(string=_('Description'))
 
@@ -21,6 +21,7 @@ class CatalogPhasesRel(models.Model):
 
     _name = 'catalog.phase'
 
+    project_id = fields.Many2one('project.project', string=_('Project'), )
     phase_id = fields.Many2one('catalog.phases', string=_('Phase'))
     date_contract = fields.Date(string=_('Contract Date'))
     revenue = fields.Float(string=_('Revenue'))
